f69f940fe49afd36a90fe313d0097f149c9e973a
Ryosuke Sato <ryz310@gmail.com>
2017-02-26 00:34:24 +0900

    create error serializer and spec

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,12 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :body
+
+  class ErrorSerializer < ActiveModel::Serializer
+    attributes :title
+
+    def title
+      object.valid?
+      object.errors[:title]
+    end
+  end
 end
--- spec/serializers/post_serializer_spec.rb
+++ spec/serializers/post_serializer_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 RSpec.describe PostSerializer, type: :serializer do
   subject(:serializer) { PostSerializer.new(post) }
-  let(:post) { FactoryGirl.build(:post) }
 
-  it do
-    expect(serializer.to_json).to eq(
-      {
-        id: post.id,
-        title: post.title,
-        body: post.body,
-      }.to_json
-    )
+  context 'when validated' do
+    let(:post) { FactoryGirl.build(:post) }
+    it do
+      expect(serializer.to_json).to eq(
+        {
+          id: post.id,
+          title: post.title,
+          body: post.body
+        }.to_json
+      )
+    end
+  end
+
+  context 'when invalidated' do
+    let(:post) { FactoryGirl.build(:post, title: nil) }
+    it do
+      error = PostSerializer::ErrorSerializer.new(post)
+      expect(error.to_json).to eq(
+        {
+          title: ['can\'t be blank'],
+        }.to_json
+      )
+    end
   end
 end
